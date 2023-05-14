ae7aa0f9b19e8554c37895b8e6dc0bb4c3c19ab5
Rebecca C <20203tn077@utez.edu.mx>
2023-05-14 12:59:49 -0600

    Finalización de ejemplo

---
+++ ConsoleUtils.cs
@@ -0,0 +1,186 @@
+﻿namespace ExampleCSharpConsoleApp;
+
+public static class ConsoleUtils
+{
+    private const int ConsoleWidth = 119;
+    private const int SleepDelay = 1000;
+    private const bool DefaultClean = false;
+    private const ConsoleKey ConfirmationKey = ConsoleKey.Y;
+    private const ConsoleKey RejectionKey = ConsoleKey.N;
+    private const string StartMessage = "Presiona cualquier tecla para continuar...";
+    private const string OptionSelectionMessage = "Selecciona una opción";
+    private const string EmptyInputMessage = "Entrada inválida, debes escribir algo";
+    private const string InvalidIntMessage = "Entrada inválida, debes ingresar un número entero";
+    private const string InvalidDoubleMessage = "Entrada inválida, debes ingresar un número decimal";
+    private const string InvalidOptionMessage = "Opción inválida";
+
+    public static void Greeting(string title, string summary)
+    {
+        var bar = String.Concat(Enumerable.Repeat('=', ConsoleWidth));
+        if (DefaultClean) Console.Clear();
+        Console.WriteLine(
+            $"""
+            {bar}
+            {AddPadding(title)}
+            {bar}
+            
+            {summary}
+
+            {StartMessage}
+
+            """
+        );
+        Console.ReadKey(true);
+        if (DefaultClean) Console.Clear();
+    }
+
+    public static void Farewell(string message)
+    {
+        if (DefaultClean) Console.Clear();
+        Console.WriteLine(AddPadding(message));
+        Thread.Sleep(SleepDelay);
+    }
+
+    public static string ReadString(string message)
+    {
+        var response = "";
+        bool validInput;
+
+        do
+        {
+            var input = InnerReadString(message);
+            validInput = input != null;
+            if (validInput) response = input ?? "";
+            else Warning(EmptyInputMessage);
+        } while (!validInput);
+
+        return response;
+    }
+
+    public static int ReadInt(string message)
+    {
+        var response = 0;
+        bool validInput;
+
+        do
+        {
+            var input = InnerReadInt(message);
+            validInput = input != null;
+            if (validInput) response = input ?? 0;
+            else Warning(InvalidIntMessage);
+        } while (!validInput);
+
+        return response;
+    }
+
+    public static double ReadDouble(string message)
+    {
+        var response = 0.0;
+        bool validInput;
+
+        do
+        {
+            var input = InnerReadDouble(message);
+            validInput = input != null;
+            if (validInput) response = input ?? 0;
+            else Warning(InvalidDoubleMessage);
+        } while (!validInput);
+
+        return response;
+    }
+
+    public static T ReadOption<T>(string message, T[] options)
+    {
+        message += ":\n";
+        for (int i = 0; i < options.Length; i++) message += $"{i + 1}.- {options[i]}\n";
+        message += $"\n{OptionSelectionMessage}";
+        bool validInput;
+        var response = options[0];
+
+        do
+        {
+            var input = InnerReadInt(message) ?? 0;
+            validInput = (input >= 1 && input <= options.Length);
+            if (validInput) response = options[input - 1];
+            else Warning(InvalidOptionMessage);
+        } while (!validInput);
+
+        return response;
+    }
+
+    public static bool Confirm(string message)
+    {
+        var response = false;
+        bool validInput;
+
+        do
+        {
+            Print($"{message} [{ConfirmationKey}/{RejectionKey}]");
+            var input = Console.ReadKey(true);
+            validInput = input.Key is ConfirmationKey or RejectionKey;
+            if (validInput) response = input.Key == ConfirmationKey;
+            else Warning(InvalidOptionMessage);
+        } while (!validInput);
+
+        return response;
+    }
+
+    public static void Info(string message) => Info(message, DefaultClean);
+
+    public static void Warning(string message) => Warning(message, DefaultClean);
+
+    public static void Info(string message, bool clearAfter) => Alert(AddPadding(message), clearAfter);
+
+    public static void Warning(string message, bool clearAfter) => Alert(AddPadding($"[!] {message} [!]"), clearAfter);
+
+    public static void Alert(string message)
+    {
+        Alert(message, DefaultClean);
+    }
+
+    public static void Alert(string message, bool clearAfter)
+    {
+        Print(message);
+        Thread.Sleep(SleepDelay);
+        if (clearAfter) Console.Clear();
+    }
+
+    private static string? InnerReadString(string message)
+    {
+        Console.WriteLine($"{message}:");
+        var value = InnerReadLine();
+        if (!string.IsNullOrWhiteSpace(value)) return value;
+        return null;
+    }
+
+    private static int? InnerReadInt(string message)
+    {
+        Console.WriteLine($"{message}:");
+        if (int.TryParse(InnerReadLine(), out var value)) return value;
+        return null;
+    }
+
+    private static double? InnerReadDouble(string message)
+    {
+        Console.WriteLine($"{message}:");
+        if (double.TryParse(InnerReadLine(), out var value)) return value;
+        return null;
+    }
+
+    private static string? InnerReadLine()
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine();
+        return input;
+    }
+
+    public static void Print(string message) => Console.WriteLine($"{message}\n");
+
+    private static string AddPadding(string message)
+    {
+        const char padChar = '-';
+        message = $" {message} ";
+        var spaceEnd = (ConsoleWidth - message.Length) / 2;
+        return message.PadLeft(ConsoleWidth - spaceEnd, padChar).PadRight(ConsoleWidth, padChar);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ExampleCSharpConsoleApp;
+
+public class ExampleApp
+{
+    private const string Title = "EJEMPLO DE APLICACIÓN EN CONSOLA";
+
+    private const string Summary = (
+        """
+        Este es un ejemplo de aplicación en consola hecho con C#.
+        En esta aplicación, podrás probar algunas de las funciones de utilidades de consola que tiene la clase ConsoleUtils. Las utilidades abarcan principalmente validación de datos, pero se incluyen también algunas herramientas de despliegue de información en pantalla. 
+        """
+    );
+
+    private static readonly string[] Strings = { "Opción 1", "Opción 2", "Opción 3", "Opción 4", "Opción 5" };
+    private static readonly int[] Ints = { 2, 3, 5, 7, 11, 13, 17 };
+    private static readonly double[] Doubles = { -23.0, 0.1, 7.9 };
+
+    public static void Main()
+    {
+        ConsoleUtils.Greeting(Title, Summary);
+        
+        do
+        {
+            ConsoleUtils.Print("Lectura de texto");
+            var stringInput = ConsoleUtils.ReadString("Ingresa algo de texto");
+            ConsoleUtils.Alert($"Texto ingresado: {stringInput}");
+            
+            ConsoleUtils.Print("Lectura de números enteros");
+            var intInput = ConsoleUtils.ReadInt("Ingresa un número entero");
+            ConsoleUtils.Alert($"Número ingresado: {intInput}");
+            
+            ConsoleUtils.Print("Lectura de números decimales");
+            var doubleInput = ConsoleUtils.ReadDouble("Ingresa un número decimal");
+            ConsoleUtils.Alert($"Número ingresado: {doubleInput}");
+            
+            ConsoleUtils.Print("Lectura de texto");
+            var stringOption = ConsoleUtils.ReadOption("Opciones de texto", Strings);
+            ConsoleUtils.Alert($"Texto seleccionado: {stringOption}");
+            
+            ConsoleUtils.Print("Lectura de números enteros");
+            var intOption = ConsoleUtils.ReadOption("Opciones de números enteros", Ints);
+            ConsoleUtils.Alert($"Número seleccionado: {intOption}");
+            
+            ConsoleUtils.Print("Lectura de números decimales");
+            var doubleOption = ConsoleUtils.ReadOption("Opciones de números decimales", Doubles);
+            ConsoleUtils.Alert($"Número seleccionado: {doubleOption}");
+            
+            ConsoleUtils.Print("Confirmación de acciones");
+        } while (!ConsoleUtils.Confirm("¿Deseas salir de la aplicación?"));
+
+        ConsoleUtils.Farewell("¡Adiós!");
+    }
+}
